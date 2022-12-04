0a8c27a6e2f5d0b6b76f62f5c08e6f1ed93f3145
RuslanDakeshev <bil08@outlook.com>
2022-12-04 01:20:59 +0100

    g

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,1 +1,22 @@
+import React, { Component } from 'react';
 
+
+
+
+export class App extends Component {
+  state = {
+    id: '',
+    
+  }
+
+
+  render() {
+    return {
+      <> 
+    
+      </>
+    }
+  }
+}
+
+export default App;
